411de45a82f24e89ff1087e731d8f55a17df3526
whary11 <whary11@gmail.com>
2018-07-13 11:05:50 -0500

    Se agregó la funcionalidad de tiempo real

---
+++ sistema-reserva-menus/public/js/config.js
@@ -0,0 +1,11 @@
+let config = {
+    apiKey: "AIzaSyCBmTdh3gfy4SRrNxDXOmV53rpk43NtQ90",
+    authDomain: "enredes-f8244.firebaseapp.com",
+    databaseURL: "https://enredes-f8244.firebaseio.com",
+    projectId: "enredes-f8244",
+    storageBucket: "enredes-f8244.appspot.com",
+    messagingSenderId: "870210169558"
+  };
+firebase.initializeApp(config);
+let db = firebase.database();
+let ref = db.ref('restaurante/');
---
+++ sistema-reserva-menus/resources/views/pdf/pedidos.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Menús Creados PDF</title>
+  <link rel="stylesheet" type="text/css" href="css/pdf.css">
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+</head>
+
+
+<body class="vista-usuario">
+  <div class="container">
+    <div class="logo-pdf">
+      <img src="img/logo.png" alt="">
+    </div>
+    <div class="titulo-menus-solicitados">
+      <h2>Menus Solicitados Hoy</h2>
+    </div>
+    <table class="datos-seleccion-usuarios">
+      <thead>
+        <tr>
+          <th>Nombre Usuario</th>
+          <th>Dependencia</th>
+          <th>Teléfono</th>
+          <th>Correo electrónico</th>
+          <th>Menú Seleccionado</th>
+          <th>Adicional</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>cell1_1</td>
+          <td>cell2_1</td>
+          <td>cell3_1</td>
+          <td>cell4_1</td>
+          <td>cell5_1</td>
+          <td>cell6_1</td>
+        </tr>
+        <tr>
+          <td>cell1_2</td>
+          <td>cell2_2</td>
+          <td>cell3_2</td>
+          <td>cell4_2</td>
+          <td>cell5_2</td>
+          <td>cell6_2</td>
+        </tr>
+        <tr>
+          <td>cell1_3</td>
+          <td>cell2_3</td>
+          <td>cell3_3</td>
+          <td>cell4_3</td>
+          <td>cell5_3</td>
+          <td>cell6_3</td>
+        </tr>
+        <tr>
+          <td>cell1_4</td>
+          <td>cell2_4</td>
+          <td>cell3_4</td>
+          <td>cell4_4</td>
+          <td>cell5_4</td>
+          <td>cell6_4</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+
+
+</body>
+
+</html>
