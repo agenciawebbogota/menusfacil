fd7f8782a328587067c9a2d655134ca30a75c02d
whary11 <whary11@gmail.com>
2018-07-15 10:05:02 -0500

    Se modificó la vista para el inicio de sesión

--- sistema-reserva-menus/resources/views/auth/login.blade.php
+++ sistema-reserva-menus/resources/views/auth/login.blade.php
@@ -1,72 +1,66 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>inicio</title>
+  <link href="{{ url('css/login.css') }}" rel="stylesheet">
+  <link href="{{ url('css2/materialize.min.css') }}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+<body>
 
-@section('content')
-<div class="container" id="login">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+  <div id="login-page" class="row">
+    <div class="col s8 l4 z-depth-4 offset-l4 offset-s2 card-panel">
+      <form class="login-form col l8 offset-l2" method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}" " id="login">
+        @csrf
+        <div class="row">
+          <div class="input-field col l12 s10 m12 offset-s2 offset-m2 center">
+            <img src="{{url('img/logo.png')}}" alt="" class="circle responsive-img valign profile-image-login">
+            <p class="center login-form-text">MenusFácil</p>
+          </div>
+      
+          <div class="input-field col l12 s10 m12 offset-s2 offset-m2">
+            <i class="material-icons prefix">person</i>
+            <input id="email" type="email" class="validate" name="email" value="{{ old('email') }}">
+            <label for="email">Correo</label>
+             @if ($errors->has('email'))
+              <span class="red-text">{{ $errors->first('email') }}</span>
+            @endif
+          </div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}" aria-label="{{ __('Login') }}" @submit.prevent="login()" id="login">
-                        @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus v-model="datos.email">
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required v-model="datos.password">
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+          <div class="input-field col l12 s10 m12 offset-s2 offset-m2">
+            <i class="material-icons prefix">lock</i>
+            <input id="password" type="password" name="password">
+            <label for="password">Contraseña</label>
+            @if ($errors->has('password'))
+              <span class="red-text">{{ $errors->first('password') }}</span>
+            @endif
+          </div>
+                  
+          <div class="input-field col l12 s10 m12 offset-s2 offset-m2 login-text switch">
+              <div class="switch">
+                <label>
+                  <span style="margin-left: 55px">Recordarme</span>
+                  <input type="checkbox" name="remenber" {{ old('remember') ? 'checked' : '' }}>
+                  <span class="lever"></span>
+                </label>
+              </div>
+          </div>
+            <button type="submit" class="btn btn-primary col l12 s10 m12 offset-s2 offset-m2">
+                Iniciar
+            </button>        
+          <div class="input-field col s4 l6 offset-s2">
+            <p class="medium-small"><a href="{{ route('register') }}">Registrate Ahora!</a></p>
+          </div>
+          <div class="input-field col s4 l6 offset-s2">
+              <p class="right-align medium-small"><a href="{{ route('password.request')}}">Perdiste tu clave ?</a></p>
+          </div>          
         </div>
-    </div>
 
-    {{-- @{{$data.datos}} --}}
-</div>
-<script type="text/javascript" src=""></script>
-@endsection
+      </form>
+    </div>
+  </div>
+  <script src="{{ url('js2/materialize.min.js') }}" defer></script>
+</body>
+</html>
