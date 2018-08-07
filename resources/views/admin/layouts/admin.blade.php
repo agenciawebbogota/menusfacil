84bb6127f967198f269e188a29630409cba2775c
AWBog <agenciawebbogota@gmail.com>
2018-08-07 17:58:20 -0500

    corrigiendo pdfs

--- resources/views/admin/layouts/admin.blade.php
+++ resources/views/admin/layouts/admin.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <title>Admin nuevo diseño</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <link href="/admin/css/app.css" type="text/css" rel="stylesheet" media="screen,projection">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
