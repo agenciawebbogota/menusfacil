520868a8af87ff869bd40bad133841bc7bd02824
AWBog <agenciawebbogota@gmail.com>
2018-09-02 20:24:13 -0500

    ----

--- resources/views/admin/includes/pago.blade.php
+++ resources/views/admin/includes/pago.blade.php
@@ -44,25 +44,23 @@ buyerEmail: test@test.com --}}
   <div class="img-pago">
     <!-- Boton PayU -->
     <form method="post" action="https://gateway.payulatam.com/ppp-web-gateway/pb.zul" accept-charset="UTF-8">
-      <input type="image" border="0" alt="" src="http://agenciawebbogota.com/social/boton-pagar.png" onClick="this.form.urlOrigen.value = window.location.href;"/>
-      <input name="buttonId" type="hidden" value="iTBmFbx+1Fyy7Pe4hIPsFPE3bIVkltlKm5huATupPoVNFmQFfNDpug=="/>
-      <input name="merchantId" type="hidden" value="644473"/>
-      <input name="accountId" type="hidden" value="646874"/>
-      <input name="description" type="hidden" value="Suscripción mensual MenúsFácil"/>
-      <input name="referenceCode" type="hidden" value="01"/>
-      <input name="amount" type="hidden" value="87000"/>
-      <input name="tax" type="hidden" value="0"/>
-      <input name="taxReturnBase" type="hidden" value="0"/>
-      <input name="currency" type="hidden" value="COP"/>
-      <input name="lng" type="hidden" value="es"/>
-      <input name="test" value="1" type="hidden">
-      <input name="approvedResponseUrl" type="hidden" value="{{url('confirmacion/suscripcion-exitosa')}}"/>
-      <input name="declinedResponseUrl" type="hidden" value="{{url('/confirmacion/repuesta')}}"/>
-      <input name="pendingResponseUrl" type="hidden" value="{{url('confirmacion/suscripcion-pendiente')}}"/>
-      <input name="displayShippingInformation" type="hidden" value="NO"/>
-      <input name="sourceUrl" id="urlOrigen" value="" type="hidden"/>
-      <input name="buttonType" value="SIMPLE" type="hidden"/>
-      <input name="signature" value="1c1c179edf191058f5a166ef2230127d6b8eb258f822977af9c5782963bbc122" type="hidden"/>
+      <input type="image" border="0" alt="" src="http://agenciawebbogota.com/social/boton-pagar.png" onClick="this.form.urlOrigen.value = window.location.href;"/>
+      <input name="buttonId" type="hidden" value="iTBmFbx+1Fyy7Pe4hIPsFPE3bIVkltlKm5huATupPoVNFmQFfNDpug=="/>
+      <input name="merchantId" type="hidden" value="644473"/>
+      <input name="accountId" type="hidden" value="646874"/>
+      <input name="description" type="hidden" value="Suscripción mensual MenúsFácil"/>
+      <input name="referenceCode" type="hidden" value="01"/>
+      <input name="amount" type="hidden" value="87000.00"/>
+      <input name="tax" type="hidden" value="0.00"/>
+      <input name="taxReturnBase" type="hidden" value="0.00"/>
+      <input name="currency" type="hidden" value="COP"/>
+      <input name="lng" type="hidden" value="es"/>
+      <input name="approvedResponseUrl" type="hidden" value="{{url('confirmacion/suscripcion-exitosa')}}"/>
+      <input name="declinedResponseUrl" type="hidden" value="{{url('confirmacion/fallo')}}"/>
+      <input name="pendingResponseUrl" type="hidden" value="{{url('confirmacion/suscripcion-pendiente')}}"/>
+      <input name="sourceUrl" id="urlOrigen" value="" type="hidden"/>
+      <input name="buttonType" value="SIMPLE" type="hidden"/>
+      <input name="signature" value="1c1c179edf191058f5a166ef2230127d6b8eb258f822977af9c5782963bbc122" type="hidden"/>
     </form>
     <!-- Fin Boton -->
   </div>
