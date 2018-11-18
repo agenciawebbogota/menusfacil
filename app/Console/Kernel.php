2335f59f02eab5361a0ccebc4282297396173c52
whary11 <whary11@gmail.com>
2018-11-18 15:54:12 -0500

    Probando el sistema de suscripción automatizado

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,9 @@
 
 namespace App\Console;
 use App\Menu;
+use App\User;
+use Mail;
+use DateTime;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -25,16 +28,68 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->call(function () {
-            Menu::create([
-                'user_id' => 2,
-                'nombre' => 'Lechugas de pan',
-                'descripcion' => 'Tarea Programada desde Crom',
-                'precio' => 40000,
-                'adicional' => 'NO',
-                'estado'=> 1,
-                'imagen' => 'http://res.cloudinary.com/menusfacil/image/upload/v1537344219/menusfacil/htdrzlilkx93ikehylpt.jpg',
-            ]);
-        })->timezone('America/Bogota')->dailyAt('15:45');
+            // Inicio de sentencias para el bloqueo de los usuarios al momento de la suscripción.
+          $usuarios = User::all()->where('tipo_usuario','!=', 'SUPERUSUARIO');
+          foreach ($usuarios as $usuario) {
+            // Verificar si el usuario está bloqueado
+            if($usuario->bloqueado == "SI"){
+              return 'El usuario está bloqueado';
+            }else{
+              $inicio = $usuario->fecha_creacion;
+              $fin = date("Y/m/d");
+              // dd($fin);
+              $dias = $this->diferenciaDias($inicio, $fin);
+              // Actualización en la base de datos
+              $nuevosdias = $usuario->dias - 1;
+              // Controlar que sólo sea actualizado una vez al día, programar en el cliente un reloj que ejecute esta acción a las 10 am
+              User::where('id', $usuario->id)->update([
+                'dias' => $nuevosdias,
+        			]);
+              $dias = User::where('id','=', $usuario->id)->first()->dias;
+              if($dias == 3){
+                // Notificar a todos los uausarios que le quedan tres días para que su suscripción sea bloqueada.
+                Mail::send('correos/alcumplir11dias', [
+                  'usuario' => $usuario,
+                  'dias' => $dias,
+                ], function($msj)
+                {
+                  $msj->subject('Recordatorio Ménus Fácil');
+                  $msj->to($this->email);
+                  $msj->bcc(['whary11@gmail.com', 'pablomart81@gmail.com']);
+                });
+                return 'Se envío correo electrónico informando a la empresa que está llegando a la caducidad de la suscripción., han pasado '.$dias.' días.';
+              }else if($dias == 2){
+                Mail::send('correos/alcumplir11dias', [
+                  'usuario' => $usuario,
+                  'dias' => $dias,
+                ], function($msj)
+                {
+                  $msj->subject('Recordatorio Ménus Fácil');
+                  $msj->to($this->email);
+                  $msj->bcc(['whary11@gmail.com', 'pablomart81@gmail.com']);
+                });
+                return 'Se envío correo electrónico informando a la empresa que está llegando a la caducidad de la suscripción., han pasado '.$dias.' días.';
+              }else if($dias == 1){
+                Mail::send('correos/alcumplir11dias', [
+                  'usuario' => $usuario,
+                  'dias' => $dias,
+                ], function($msj)
+                {
+                  $msj->subject('Recordatorio Ménus Fácil');
+                  $msj->to($this->email);
+                  $msj->bcc(['whary11@gmail.com', 'pablomart81@gmail.com']);
+                });
+                return 'Se envío correo electrónico informando a la empresa que está llegando a la caducidad de la suscripción., han pasado '.$dias.' días.';
+              }else if($dias == 0){
+                User::where('id', $usuario->id)->update([
+                  'bloqueado' => 'SI',
+                  'dias' => 0,
+                ]);
+              }
+        }
+      }
+        // FIn de sentencias para el bloqueo de los usuarios al momento de la suscripción, sólo pasar estas instrucciones en donde se quieren ejecutar y tomar las acciones indicadas en los echos
+        })->timezone('America/Bogota')->dailyAt('16:00');
     }
 
     /**
--- app/Http/Controllers/SuperusuarioController.php
+++ app/Http/Controllers/SuperusuarioController.php
@@ -37,78 +37,10 @@ class SuperusuarioController extends Controller
     }
   }
 
+
+
   public function actualizardias(){
-    $user_id = \Auth::id();
-    $user = User::where('id', '=', $user_id)->first();
-    // if($user->tipo_usuario === 'SUPERUSUARIO'){
-      if($user->tipo_usuario === 'SUPERUSUARIO'){
-      // Inicio de sentencias para el bloqueo de los usuarios al momento de la suscripción.
-          $usuarios = User::all()->where('id','!=', $user_id);
-
-          foreach ($usuarios as $usuario) {
-            // Verificar si el usuario está bloqueado
-            if($usuario->bloqueado == "SI"){
-              return 'El usuario está bloqueado';
-            }else{
-              $inicio = $usuario->fecha_creacion;
-              $fin = date("Y/m/d");
-              // dd($fin);
-              $dias = $this->diferenciaDias($inicio, $fin);
-              // Actualización en la base de datos
-              $nuevosdias = $usuario->dias - 1;
-
-              // Controlar que sólo sea actualizado una vez al día, programar en el cliente un reloj que ejecute esta acción a las 10 am
-              User::where('id', $usuario->id)->update([
-                'dias' => $nuevosdias,
-        			]);
-              $dias = User::where('id','=', $usuario->id)->first()->dias;
-              if($dias == 3){
-                // Notificar a todos los uausarios que le quedan tres días para que su suscripción sea bloqueada.
-                Mail::send('correos/alcumplir11dias', [
-                  'usuario' => $usuario,
-                  'dias' => $dias,
-                ], function($msj)
-                {
-                  $msj->subject('Recordatorio Ménus Fácil');
-                  $msj->to($this->email);
-                  $msj->bcc(['whary11@gmail.com', 'pablomart81@gmail.com']);
-                });
-                return 'Se envío correo electrónico informando a la empresa que está llegando a la caducidad de la suscripción., han pasado '.$dias.' días.';
-              }else if($dias == 2){
-                Mail::send('correos/alcumplir11dias', [
-                  'usuario' => $usuario,
-                  'dias' => $dias,
-                ], function($msj)
-                {
-                  $msj->subject('Recordatorio Ménus Fácil');
-                  $msj->to($this->email);
-                  $msj->bcc(['whary11@gmail.com', 'pablomart81@gmail.com']);
-                });
-                return 'Se envío correo electrónico informando a la empresa que está llegando a la caducidad de la suscripción., han pasado '.$dias.' días.';
-              }else if($dias == 1){
-                Mail::send('correos/alcumplir11dias', [
-                  'usuario' => $usuario,
-                  'dias' => $dias,
-                ], function($msj)
-                {
-                  $msj->subject('Recordatorio Ménus Fácil');
-                  $msj->to($this->email);
-                  $msj->bcc(['whary11@gmail.com', 'pablomart81@gmail.com']);
-                });
-                return 'Se envío correo electrónico informando a la empresa que está llegando a la caducidad de la suscripción., han pasado '.$dias.' días.';
-              }else if($dias == 0){
-                User::where('id', $usuario->id)->update([
-                  'bloqueado' => 'SI',
-                  'dias' => 0,
-                ]);
-              }
-        }
-      }
-        // FIn de sentencias para el bloqueo de los usuarios al momento de la suscripción, sólo pasar estas instrucciones en donde se quieren ejecutar y tomar las acciones indicadas en los echos
-
-    }else{
-      return redirect('/dashboard');
-    }
+      // 
   }
 
   public function updateUser(Request $request){
