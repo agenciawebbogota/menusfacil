78e79d76af2aa590f4b21113f18c89994003a761
whary11 <whary11@gmail.com>
2018-11-27 20:47:24 -0500

    Preparando vista de personalización de los clientes..

---
+++ resources/views/admin/personalizacion.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <title>Actualizar Cuenta || Menús Fácil</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+  <link rel="stylesheet" href="/personalizar/css/app.min.css">
+  <link rel="stylesheet" href="/personalizar/css/app.css">
+  <link rel="icon" href="/admin/img/favicon.png" sizes="32x32">
+  <style>
+      body {
+        display: flex;
+        min-height: 100vh;
+        flex-direction: column;
+    }
+    main {
+        flex: 1 0 auto;
+    }
+  </style>
+</head>
+<body>
+    <main class="">
+      <div>
+      <nav>
+        <a class="brand-logo center">
+          <img src="/admin/img/logo-menusfacil.svg" alt="Menus Facil Logo" width="100px" class="imagen-logo">
+        </a>
+      </nav>
+    </div>
+    <section class="ptb-30 gray-bg">
+        {{-- Contenido --}}
+
+
+<p>Aquí va el congtenido..</p>
+
+
+
+
+
+    </section>
+
+
+
+    <footer class="page-footer" style="background-color:#2a2730">
+        <div class="footer-copyright">
+            <div class="container">
+                <span class="text-center">Todos los derechos reservados</span>
+            </div>
+        </div>
+    </footer>
+    </main>
+    <script type="text/javascript" src="/personalizar/js/app.min.js"></script>
+</body>
+</html>
