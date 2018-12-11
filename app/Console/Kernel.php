0bc7454822ca85e42f9e11b5eaa8da27aa728693
Luis Fernando Raga Renteria <whary11@gmail.com>
2018-12-10 21:52:29 -0500

    probando cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,68 +28,80 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->call(function () {
-            // Inicio de sentencias para el bloqueo de los usuarios al momento de la suscripción, excluimos todos los usuarios SUPERUSUARIOS.
+            // // Inicio de sentencias para el bloqueo de los usuarios al momento de la suscripción, excluimos todos los usuarios SUPERUSUARIOS.
             $usuarios = User::all()->where('tipo_usuario','!=', 'SUPERUSUARIO');
-            foreach ($usuarios as $usuario) {
-                // Verificar si el usuario está bloqueado
-                if($usuario->bloqueado == "SI"){
-                    return 'El usuario está bloqueado';
-                }else{
-                    $inicio = $usuario->fecha_creacion;
-                    $fin = date("Y/m/d");
-                    // dd($fin);
-                    $dias = $this->diferenciaDias($inicio, $fin);
-                    // Actualización en la base de datos
-                    $nuevosdias = $usuario->dias - 1;
-                    // Controlar que sólo sea actualizado una vez al día, programar en el cliente un reloj que ejecute esta acción a las 10 am
-                    User::where('id', $usuario->id)->update([
-                        'dias' => $nuevosdias,
-                            ]);
-                    $dias = User::where('id','=', $usuario->id)->first()->dias;
-                    if($dias == 3){
-                        // Notificar a todos los uausarios que le quedan tres días para que su suscripción sea bloqueada.
-                        Mail::send('correos/alcumplir11dias', [
-                        'usuario' => $usuario,
-                        'dias' => $dias,
-                        ], function($msj)
-                        {
-                        $msj->subject('Recordatorio Ménus Fácil');
-                        $msj->to($this->email);
-                        $msj->bcc(['whary11@gmail.com', 'pablomart81@gmail.com']);
-                        });
-                        return 'Se envío correo electrónico informando a la empresa que está llegando a la caducidad de la suscripción., han pasado '.$dias.' días.';
-                    }else if($dias == 2){
-                        Mail::send('correos/alcumplir11dias', [
-                        'usuario' => $usuario,
-                        'dias' => $dias,
-                        ], function($msj)
-                        {
-                        $msj->subject('Recordatorio Ménus Fácil');
-                        $msj->to($this->email);
-                        $msj->bcc(['whary11@gmail.com', 'pablomart81@gmail.com']);
-                        });
-                        return 'Se envío correo electrónico informando a la empresa que está llegando a la caducidad de la suscripción., han pasado '.$dias.' días.';
-                    }else if($dias == 1){
-                        Mail::send('correos/alcumplir11dias', [
-                        'usuario' => $usuario,
-                        'dias' => $dias,
-                        ], function($msj)
-                        {
-                        $msj->subject('Recordatorio Ménus Fácil');
-                        $msj->to($this->email);
-                        $msj->bcc(['whary11@gmail.com', 'pablomart81@gmail.com']);
-                        });
-                        return 'Se envío correo electrónico informando a la empresa que está llegando a la caducidad de la suscripción., han pasado '.$dias.' días.';
-                    }else if($dias == 0){
-                        User::where('id', $usuario->id)->update([
-                        'bloqueado' => 'SI',
-                        'dias' => 0,
-                        ]);
-                    }
-                }
-            }
+            // foreach ($usuarios as $usuario) {
+            //     // Verificar si el usuario está bloqueado
+            //     if($usuario->bloqueado == "SI"){
+            //         return 'El usuario está bloqueado';
+            //     }else{
+            //         $inicio = $usuario->fecha_creacion;
+            //         $fin = date("Y/m/d");
+            //         // dd($fin);
+            //         $dias = $this->diferenciaDias($inicio, $fin);
+            //         // Actualización en la base de datos
+            //         $nuevosdias = $usuario->dias - 1;
+            //         // Controlar que sólo sea actualizado una vez al día, programar en el cliente un reloj que ejecute esta acción a las 10 am
+            //         User::where('id', $usuario->id)->update([
+            //             'dias' => $nuevosdias,
+            //                 ]);
+            //         $dias = User::where('id','=', $usuario->id)->first()->dias;
+            //         if($dias == 3){
+            //             // Notificar a todos los uausarios que le quedan tres días para que su suscripción sea bloqueada.
+            //             Mail::send('correos/alcumplir11dias', [
+            //             'usuario' => $usuario,
+            //             'dias' => $dias,
+            //             ], function($msj)
+            //             {
+            //             $msj->subject('Recordatorio Ménus Fácil');
+            //             $msj->to($this->email);
+            //             $msj->bcc(['whary11@gmail.com', 'pablomart81@gmail.com']);
+            //             });
+            //             return 'Se envío correo electrónico informando a la empresa que está llegando a la caducidad de la suscripción., han pasado '.$dias.' días.';
+            //         }else if($dias == 2){
+            //             Mail::send('correos/alcumplir11dias', [
+            //             'usuario' => $usuario,
+            //             'dias' => $dias,
+            //             ], function($msj)
+            //             {
+            //             $msj->subject('Recordatorio Ménus Fácil');
+            //             $msj->to($this->email);
+            //             $msj->bcc(['whary11@gmail.com', 'pablomart81@gmail.com']);
+            //             });
+            //             return 'Se envío correo electrónico informando a la empresa que está llegando a la caducidad de la suscripción., han pasado '.$dias.' días.';
+            //         }else if($dias == 1){
+            //             Mail::send('correos/alcumplir11dias', [
+            //             'usuario' => $usuario,
+            //             'dias' => $dias,
+            //             ], function($msj)
+            //             {
+            //             $msj->subject('Recordatorio Ménus Fácil');
+            //             $msj->to($this->email);
+            //             $msj->bcc(['whary11@gmail.com', 'pablomart81@gmail.com']);
+            //             });
+            //             return 'Se envío correo electrónico informando a la empresa que está llegando a la caducidad de la suscripción., han pasado '.$dias.' días.';
+            //         }else if($dias == 0){
+            //             User::where('id', $usuario->id)->update([
+            //             'bloqueado' => 'SI',
+            //             'dias' => 0,
+            //             ]);
+            //         }
+            //     }
+            // }
+
+
+            Mail::send('correos/alcumplir11dias', [
+                            'usuario' => $usuarios[0],
+                            'dias' => 10,
+                            ], function($msj)
+                            {
+                            $msj->subject('Recordatorio Ménus Fácil');
+                            $msj->to('whary11@gmail.com');
+                            $msj->bcc(['whary11@gmail.com']);
+                            });
+
             // Definir el horario y zona de ejecución
-        })->timezone('America/Bogota')->dailyAt('08:00');
+        })->timezone('America/Bogota')->dailyAt('21:53');
     }
 
     /**
